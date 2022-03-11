53d29cfebd5bfb591c48d9d513c37c1f0ba7704a
Sebastian Vargas <vargaspizango1@gmail.com>
2022-03-11 15:12:21 -0500

    chore: add weather service

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Weather } from '../models/weather';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  apiUrl = environment.apiURL;
+  apiKey = environment.apiKey;
+
+  constructor(private http: HttpClient) {}
+
+  public getWeather(city: string): Observable<Weather> {
+    const params = new HttpParams()
+      .set('units', 'metric')
+      .set('q', city)
+      .set('appId', this.apiKey);
+
+    return this.http.get<Weather>(`${this.apiUrl}weather`, { params });
+  }
+}
